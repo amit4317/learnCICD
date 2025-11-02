cd697648ee0c235b1b5d02631a43dd9a1f6fb8cc
Amit Kumar <amit.kumar@farmershoptechindia.com>
2025-11-03 03:50:27 +0530

    no message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/health", (req, res) => res.json({ ok: true }));
-app.get("/", (req, res) => res.json({ service: "api", time: new Date().toISOString() }));
+app.get("/", (req, res) => res.json({ service: "api_v2", time: new Date().toISOString() }));
 app.get("/amit", (req, res) => res.json({
            message: 'The server is running',
            createdby: 'meAmit',
